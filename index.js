34c0330668e75980d75db722af5cc60efd485100
timanlemvo <timanlemvo@gmail.com>
2021-09-14 15:00:43 -0700

    added html template and update lib and index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const employeeArr = [];
+const templateInfo = require('./src/template');
 
-const questions = [
+function employeeInfo() { 
+return inquirer.prompt([
     {
             type: 'list',
             name: 'roleChoice',
@@ -37,33 +39,70 @@ const questions = [
         type: 'input',
         name: 'github',
         message: 'enter your github username?',
-        when: answers => answers.rolechoice === 'Engineer'
+        when: answers => answers.roleChoice === 'Engineer'
     },
     {
         type: 'input',
         name: 'phone',
         message: 'what is your office number?',
         when: answers => answers.roleChoice === 'Manager'
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAgain',
+        message: 'would you like to add another employee?',
+        default: true
     }
-    ]
+])
+    .then(answers => {
+        // const employeeArr = [];
+        if(answers.roleChoice === 'Intern') {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+             employeeArr.push(intern);
+            //  console.log(employeeArr);
+            //  console.log(intern);
+         } else if (answers.roleChoice === 'Engineer') {
+            const engineer =  new Engineer(answers.name, answers.id, answers.email, answers.github);
+            // console.log(engineer);
+            employeeArr.push(engineer);
+            // console.log(employeeArr);
+         } else if (answers.roleChoice === 'Manager') {
+           const manager = new Manager(answers.name, answers.id, answers.email, answers.phone); 
+        //    console.log(manager); 
+           employeeArr.push(manager);
+        //    console.log(employeeArr);
+         }
+
+        if(answers.confirmAgain) {
+            return employeeInfo(answers);
+        } else {
+            return answers;
+        }
+        
+    });
+    
+}
 
 function initQuest() {
-    inquirer.prompt(questions).then((answers) => {
-        if(answers.roleChoice === 'Intern') {
-           const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-            employeeArr.push(intern);
-            console.log(intern);
-        } else if (answers.roleChoice === 'Engineer') {
-           const engineer =  new Engineer(answers.name, answers.id, answers.email, answers.github);
-           console.log(engineer);
-           employeeArr.push(engineer);
-        } else if (answers.roleChoice === 'Manager') {
-          const manager = new Manager(answers.name, answers.id, answers.email, answers.phone); 
-          console.log(manager); 
-          employeeArr.push(manager);
-        }
+    employeeInfo()
+    .then(() => {
+       return templateInfo(employeeArr)}
+    )
+    .then((templateInfo) => {
+        console.log(templateInfo);
+       // writeFile();
     })
 }
+
+
+function writeFile () {
+    fs.writeFile('index.html', template , (err) => {
+        if(err) throw err;
+        console.log('File Created!');
+    })
+}
+
+
 
 
 
